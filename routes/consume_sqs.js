70536124a910aa1ec1da15b6b9a0cee6c7b48f64
Mark Mastoras <mmastoras@mac.com>
2015-06-05 10:55:31 -0700

    return posted json

--- routes/consume_sqs.js
+++ routes/consume_sqs.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 /* GET home page. */
-router.post('/', function(req, res) {
-   res.send('Got a POST request');
+router.post('/', jsonParser, function(req, res) {
+   res.send(JSON.stringify(req.body, null, 2));
 });
 
 module.exports = router;
